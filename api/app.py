da7b6e2206c75cc725a22e314246110f5587bf57
Coutaq <coutaq@gmail.com>
2021-12-02 16:58:42 +0300

    broken

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from api.utils import model_to_route, model_to_route_id
 from database.MySQLConnection import MySQLConnection
+from database.models.Category import Category
 from database.models.User import User
 from log.ConsoleLogger import ConsoleLogger
 from log.LogManager import LogManager
@@ -17,13 +18,14 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-exposed_models = {"user": User}
-for name, model in exposed_models:
-    @app.route(f'/{name}', methods=['POST', 'GET'])
-    def user():
-        return model_to_route(model, conn)()
+exposed_models = {"user": User, "category": Category}
+for name, model in exposed_models.items():
+    def f1():
+        return model_to_route(model, conn)()()
+    f1.__name__ = name
 
-
-    @app.route(f'/{name}/<int:id>', methods=['GET', 'DELETE', 'PUT'])
-    def user_id(id):
-        return model_to_route_id(model, conn)(id)
+    def f2(id):
+        return model_to_route_id(model, conn)()(id)
+    f2.__name__ = name + "_id"
+    app.add_url_rule(f'/{name}', view_func=f1, methods=['POST', 'GET'])
+    app.add_url_rule(f'/{name}/<int:id>', view_func=f2, methods=['GET', 'DELETE', 'PUT'])
