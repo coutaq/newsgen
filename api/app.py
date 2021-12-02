c016152db747d41f1626fcbd8b2af5c8e83abb9e
Coutaq <7tGu9ZQn>
2021-12-02 11:50:51 +0300

    basic rest api

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from log.ConsoleLogger import ConsoleLogger
 from log.LogManager import LogManager
 from database.models.User import User
 from database.MySQLConnection import MySQLConnection
+from api.utils import model_to_route, model_to_route_id
 
 lg = LogManager()
 lg.attach(ConsoleLogger())
@@ -14,33 +15,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
 @app.route('/user', methods=['POST', 'GET'])
 def user():
-    if request.method == 'GET':
-        query = User.read()
-        data = conn.execute_query(query, True)
-    if request.method == 'POST':
-        fields = User.fields() + User.foreign_fields()
-        fields.remove("id")
-        values = [request.form.get(x) for x in fields]
-        query = User.create(values)
-        data = conn.execute_query(query, True)
-    return jsonify(data)
+    return model_to_route(User, conn)()
 
 
 @app.route('/user/<int:id>', methods=['GET', 'DELETE', 'PUT'])
 def user_id(id):
-    if request.method == 'GET':
-        query = User.read(where=id)
-        data = conn.execute_query(query, True)
-    if request.method == 'PUT':
-        fields = User.fields() + User.foreign_fields()
-        fields.remove("id")
-        values = [request.form.get(x) for x in fields]
-        query = User.update(id, values)
-        data = conn.execute_query(query, True)
-    if request.method == 'DELETE':
-        query = User.delete(id)
-        data = conn.execute_query(query, True)
-    return jsonify(data)
+    return model_to_route_id(User, conn)(id)
---
+++ api/utils.py
@@ -0,0 +1,37 @@
+from database.models.BaseModel import BaseModel
+from flask import request, jsonify
+
+
+def model_to_route(model: BaseModel, conn):
+    def model():
+        if request.method == 'GET':
+            query = model.read()
+            data = conn.execute_query(query, True)
+        if request.method == 'POST':
+            fields = model.fields() + model.foreign_fields()
+            fields.remove("id")
+            values = [request.form.get(x) for x in fields]
+            query = model.create(values)
+            data = conn.execute_query(query, True)
+        return jsonify(data)
+
+    return model
+
+
+def model_to_route_id(model: BaseModel, conn):
+    def model_id(id):
+        if request.method == 'GET':
+            query = model.read(where=id)
+            data = conn.execute_query(query, True)
+        if request.method == 'PUT':
+            fields = model.fields() + model.foreign_fields()
+            fields.remove("id")
+            values = [request.form.get(x) for x in fields]
+            query = model.update(id, values)
+            data = conn.execute_query(query, True)
+        if request.method == 'DELETE':
+            query = model.delete(id)
+            data = conn.execute_query(query, True)
+        return jsonify(data)
+
+    return model_id
