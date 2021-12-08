8946d0c0aa77c5341999e42ce57535a4b8ff488f
Coutaq <7tGu9ZQn>
2021-12-09 01:11:19 +0300

    file upload

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ from api.app import app
 import os
 basedir = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = "/assets/"
-app.config['BASEDIR'] = basedir
+app.config['BASEDIR'] = os.path.realpath("/home/coutaq/public_html/newsletter/assets")
 if __name__ == "__main__":
     app.run()
