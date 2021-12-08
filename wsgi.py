c6cb69026bf66cfcb9bed1d6d555798c45546f70
Coutaq <7tGu9ZQn>
2021-12-09 00:26:46 +0300

    file upload

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from api.app import app
-
+import os
+basedir = os.path.dirname(os.path.realpath(__file__))
+app.config['UPLOAD_FOLDER'] = "/assets/"
+app.config['BASEDIR'] = basedir
 if __name__ == "__main__":
     app.run()
