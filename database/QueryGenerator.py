8f9d7416b4fdb6e3a0578d6e54a5a9c84e38dcfd
Coutaq <7tGu9ZQn>
2021-12-10 02:22:42 +0300

    reports

--- database/QueryGenerator.py
+++ database/QueryGenerator.py
@@ -7,7 +7,7 @@ sep = '\''
 
 def create_select(table_name: str, fields, *args):
     def select(where):
-        return f"SELECT {arr_to_str(fields, '')} from {table_name} {' '.join(args)} {'WHERE id = ' + where if where else ''} ORDER BY `{table_name}`.id;"
+        return f"SELECT {arr_to_str(fields, '')} from {table_name} {' '.join(args)} {f'WHERE `{table_name}`.id = ' + where if where else ''} ORDER BY `{table_name}`.id;"
 
     return select
 
