4b775c35664db1fb10f3175813f70fe560e193ad
Coutaq <7tGu9ZQn>
2021-12-10 03:43:35 +0300

    reports

--- database/models/UserPost.py
+++ database/models/UserPost.py
@@ -18,7 +18,7 @@ class UserPost(BaseModel):
     @staticmethod
     def read(where=None, fields=None) -> str:
         if fields is None:
-            fields = ["user_posts.id, user_posts.opened, posts.title as 'post', users.name as 'user' "]
+            fields = ["user_posts.*, posts.title as 'post', users.name as 'user' "]
         return QueryGenerator.create_select(UserPost.table_name(), fields, "LEFT JOIN `posts` on post_id = posts.id", "LEFT JOIN `users` on user_id = users.id")(where)
 
     @staticmethod
