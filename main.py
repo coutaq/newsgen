8476efcca28687196ac246c043c08716f57d3336
Coutaq <7tGu9ZQn>
2021-12-11 18:03:50 +0300

    reports

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from api.app import app
 import os
 from database.models.User import User
+from database.models.UserPost import UserPost
 from database.models.Post import Post
 from database.MySQLConnection import MySQLConnection
+from random import choice
 basedir = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = "/assets/"
 app.config['BASEDIR'] = basedir
@@ -15,5 +17,10 @@ query = Post.read(fields=['posts.id'])
 posts = conn.execute_query(query, True)
 users =[d['id'] for d in users]
 posts = [d['id'] for d in posts]
-print(users)
-print(posts)
+
+for i in range(300):
+    post = choice(posts)
+    user = choice(users)
+    opened = choice(["1", "0"])
+    query = UserPost.create([post, user, opened])
+    users = conn.execute_query(query, True)
