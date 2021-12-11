59d7abbcacdde602cd806620aa93c539cd80d4a1
Coutaq <coutaq@gmail.com>
2021-12-12 02:32:01 +0300

    update

--- api/app.py
+++ api/app.py
@@ -25,7 +25,7 @@ CORS(app)
 @app.route("/")
 @auth.login_required()
 def hello_world():
-    return "Hello, {}!".format(auth.get_auth())
+    return "Hello, {}!".format(auth.current_user())
 
 
 @auth.verify_password
