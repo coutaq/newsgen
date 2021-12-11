a387239ae5525f93cb3366e6848a1d9cc28b9dfc
Coutaq <7tGu9ZQn>
2021-12-11 18:10:04 +0300

    reports

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ app.config['BASEDIR'] = basedir
 #app.run(debug=True)
 
 conn = MySQLConnection()
-query = User.read(fields=['users.id'])
-users = conn.execute_query(query, True)
-query = Post.read(fields=['posts.id'])
-posts = conn.execute_query(query, True)
-users =[d['id'] for d in users]
-posts = [d['id'] for d in posts]
+
 
 for i in range(300):
+    query = User.read(fields=['users.id'])
+    users = conn.execute_query(query, True)
+    query = Post.read(fields=['posts.id'])
+    posts = conn.execute_query(query, True)
+    users = [d['id'] for d in users]
+    posts = [d['id'] for d in posts]
     post = choice(posts)
     user = choice(users)
     opened = choice(["1", "0"])
     query = UserPost.create([opened, "test", user, post])
-    users = conn.execute_query(query, True)
+    #users = conn.execute_query(query, True)
+    print(query)
